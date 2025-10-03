68b67b826fd434abb909f2187533adb33d13abaa
JoakimSundman <130984408+JoakimSundman@users.noreply.github.com>
2025-10-03 17:38:16 +0200

    fixing files?

--- caddy-tutorial/setup.sh
+++ caddy-tutorial/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 WORKING_DIR="/root/caddy"
+ASSETS_DIR="/root/assets"
 
 echo "Setting up working directory at $WORKING_DIR (Ubuntu base)"
 
@@ -11,10 +12,10 @@ apt install -y netcat > /dev/null 2>&1
 # Create working directory and html directory for webapp
 mkdir -p $WORKING_DIR/html
 
-#Copy all prepared assets from the scenario source into the working directory
-cp assets/Dockerfile $WORKING_DIR/
-cp assets/docker-compose.yml $WORKING_DIR/
-cp assets/html/index.html $WORKING_DIR/html/
+# Copy all prepared assets from the scenario source into the working directory
+cp $ASSETS_DIR/Dockerfile $WORKING_DIR/
+cp $ASSETS_DIR/docker-compose.yml $WORKING_DIR/
+cp $ASSETS_DIR/html/index.html $WORKING_DIR/html/
 
 # Move user into working directory for scenario
 cd $WORKING_DIR
