13fdf2fdaf98fbbff80eeacea45bb348197f7777
JoakimSundman <130984408+JoakimSundman@users.noreply.github.com>
2025-10-03 18:16:03 +0200

    Update setup1

--- caddy-tutorial/setup1.sh
+++ caddy-tutorial/setup1.sh
@@ -3,7 +3,4 @@
 WORKING_DIR="/root/caddy"
 ASSETS_DIR="/root/assets"
 
-# Copy files from /root/assets to /root/caddy
-cp $ASSETS_DIR/Dockerfile $WORKING_DIR/
 cp $ASSETS_DIR/docker-compose.yml $WORKING_DIR/
-cp -r $ASSETS_DIR/html/* $WORKING_DIR/html/
