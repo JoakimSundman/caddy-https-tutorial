561ad10f5788a3f8248e0fe4b784901b8660fdf3
JoakimSundman <130984408+JoakimSundman@users.noreply.github.com>
2025-10-03 17:54:38 +0200

    added website

--- caddy-tutorial/setup.sh
+++ caddy-tutorial/setup.sh
@@ -3,16 +3,16 @@
 WORKING_DIR="/root/caddy"
 ASSETS_DIR="/root/assets"
 
-echo "Setting up working directory at $WORKING_DIR (Ubuntu base)"
+mkdir -p $ASSETS_DIR
 
 # Install netcat for security endpoint
 apt update > /dev/null 2>&1
 apt install -y netcat > /dev/null 2>&1
 
 # Create working directory and html directory for webapp
-mkdir -p $WORKING_DIR/html
-wget -O $WORKING_DIR/docker-compose.yml https://raw.githubusercontent.com/JoakimSundman/caddy-https-tutorial/main/caddy-tutorial/assets/docker-compose.yml
-wget -O $WORKING_DIR/html/index.html https://raw.githubusercontent.com/JoakimSundman/caddy-https-tutorial/main/caddy-tutorial/assets/html/index.html
+mkdir -p $WORKING_DIR/site
+wget -O $ASSETS_DIR/docker-compose.yml https://raw.githubusercontent.com/JoakimSundman/caddy-https-tutorial/main/caddy-tutorial/assets/docker-compose.yml
+wget -O $WORKING_DIR/site/index.html https://raw.githubusercontent.com/JoakimSundman/caddy-https-tutorial/main/caddy-tutorial/assets/html/index.html
 
 # Move user into working directory for scenario
 cd $WORKING_DIR
